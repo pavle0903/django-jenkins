042047e860448a594afc502c5b0e803c56947e91
Pavle9 <psarenac@gmail.com>
2023-02-11 13:00:04 -0600

    secret key

--- app/app/settings.py
+++ app/app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from ..utils.util import kljuc
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--xvbundvo99rhw=mt6rlqq_7z3syw41z1a@*4*3+5(2la^0^ur'
+SECRET_KEY = kljuc
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
