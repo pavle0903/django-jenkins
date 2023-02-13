9f6173cf68848fc89cfa8bb26d74e5789640ad3a
Pavle9 <psarenac@gmail.com>
2023-02-13 13:42:16 -0600

    edited views

--- app/tasks/views.py
+++ app/tasks/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Hello world django app')
+    return HttpResponse('Hello world ovo je django app')
